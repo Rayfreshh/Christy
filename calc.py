4070cc2fe833999800c99f8ae60ab6a792c3ccad
Samer Ahmed <hello@samer.co.ke>
2022-09-08 08:46:26 +0200

    back to the start for masters sse

--- calc.py
+++ calc.py
@@ -1,12 +1,12 @@
 #Basic 2-number Calculator Functions
 def add(x,y):
-	return x+y
+	pass
 
 def multiply(x,y):
-	return x*y
+	pass
 
 def subtract(x,y):
-	return x-y
+	pass
 
 def divide(x,y):
-	return x/y
+	pass
